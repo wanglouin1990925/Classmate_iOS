d726e70e37f7082ef915f4428964733698c8180d
John Riccione <johnriccione@Johns-MacBook-Pro-2.local>
2018-07-22 18:03:33 -0500

    enterEmailTest file setup

---
+++ ClassmateTests/EnterEmailViewControllerTests.swift
@@ -0,0 +1,55 @@
+//
+//  EnterEmailViewControllerTests.swift
+//  ClassmateTests
+//
+//  Created by John Riccione on 7/22/18.
+//  Copyright © 2018 Administrator. All rights reserved.
+//
+
+import XCTest
+
+class EnterEmailViewControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testemptyfieldthrowserror() {
+        
+    }
+    
+    func testnonemailtextthrowserror() {
+        
+    }
+    
+    func testnonpurdueemailthrowserror() {
+        
+    }
+    
+    func testnewpurdueemailgoestoregistration() {
+        
+    }
+    
+    func testoldpurdueemailgoestologin() {
+        
+    }
+    
+    func testcheckpasswordchangeisduegoestonewpasswordscreen() {
+        
+    }
+    
+    func testcheckpasswordchangeisnotdue() {
+        
+    }
+    
+    func testleadingortrailingspacesareeliminated() {
+        
+    }
+    
+}
